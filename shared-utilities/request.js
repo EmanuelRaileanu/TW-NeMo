e8cd4a861f624fe885077b1d4538bd5c123e18a3
emanuel <emanuel.raileanu@sprybase.com>
2021-05-12 10:39:24 +0300

    Fixed request wrapper

--- shared-utilities/request.js
+++ shared-utilities/request.js
@@ -2,7 +2,7 @@ const [http, https] = [require('http'), require('https')]
 
 const request = async (url, method = 'GET', requestBody) => {
     const lib = url.startsWith('https://') ? https : http
-    const [host, path] = url.split('://')[1].split('/')
+    const [host, path] = [url.split('://')[1].split('/')[0], url.split('://')[1].substr(url.split('://')[1].indexOf('/'))]
     const [, port] = host.split(':')
     const params = {
         method,
@@ -10,6 +10,7 @@ const request = async (url, method = 'GET', requestBody) => {
         port: port || url.startsWith('https://') ? 443 : 80,
         path: path || '/'
     }
+    console.log(params)
     return new Promise((resolve, reject) => {
         const req = lib.request(params, res => {
             if (res.statusCode < 200 || res.statusCode >= 300) {
